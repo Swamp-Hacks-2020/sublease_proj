72acba2697c05728769ebfe29b6158b64aa0665b
rivera-fernando <f.rivera@ufl.edu>
2020-02-01 10:04:23 -0500

    changed organization of the form from single column to more concise layout

--- sublease/proj/forms.py
+++ sublease/proj/forms.py
@@ -11,7 +11,7 @@ class getApartmentInfo(forms.Form):
     street2 = forms.CharField(label = 'Street 2', widget = forms.TextInput(attrs = {'placeholder' : 'Apt, Bldg, Suite, etc.'}), required = False)
     city = forms.CharField(label = 'City', widget = forms.TextInput(attrs = {'placeholder' : 'Metropolis'}))
     zip = forms.CharField(label = 'Zip', widget = forms.TextInput(attrs = {'placeholder' : '00000'}), max_length = 5)
-    semester = forms.CharField(label = 'Semester', widget = forms.TextInput(attrs = {'placeholder' : 'ex: Fall 2020, Spring 2020, Summer 2020'}), max_length = 15)
+    semester = forms.CharField(label = 'Semester', widget = forms.TextInput(attrs = {'placeholder' : 'Semester Availabile (i.e. Fall 2020, Spring 2020, Summer 2020)'}), max_length = 15)
     complex = forms.CharField(label = 'Complex', widget = forms.TextInput(attrs = {'placeholder' : 'Apartment Complex'}))
     bedrooms = forms.CharField(label = 'Bedrooms', widget = forms.TextInput(attrs = {'placeholder' : 'Number of Bedrooms'}), max_length = 1)
     bathrooms = forms.CharField(label = 'Bathrooms', widget = forms.TextInput(attrs = {'placeholder' : 'Number of Bathrooms'}), max_length = 1)
