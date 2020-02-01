83ac127c0d060985e2dae259c778ac24823ecef2
ZonshenYu <zonshenyu@gmail.com>
2020-02-01 00:23:08 -0500

    database setup

--- sublease/proj/views.py
+++ sublease/proj/views.py
@@ -4,7 +4,20 @@ from pprint import pprint
 
 client = MongoClient("mongodb+srv://guest:root@listings-gru4r.mongodb.net/test?retryWrites=true&w=majority")
 
-db = client.admin
+db = client.business
 
-serverStatusResult = db.command("serverStatus")
-pprint(serverStatusResult)
+#serverStatusResult = db.command("serverStatus")
+#pprint(serverStatusResult)
+
+testListing = {
+
+	'listing_id' : "00000001",
+	'apartment_name' : "Gainesville Place",
+	'num_bedrooms' : 4,
+	
+	'lister_name' : "MingYEEN" 
+
+}
+
+result = db.listings.insert_one(testListing)
+
