1dc99d6e16801a477872457e365bda9be0d8a4d8
rivera-fernando <f.rivera@ufl.edu>
2020-02-01 00:49:12 -0500

    basic navigation with buttons and created the html files we will be using for both options

--- sublease/proj/views.py
+++ sublease/proj/views.py
@@ -1,23 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse
 from pymongo import MongoClient
 
 from pprint import pprint
 
 client = MongoClient("mongodb+srv://guest:root@listings-gru4r.mongodb.net/test?retryWrites=true&w=majority")
-
 db = client.business
 
 #serverStatusResult = db.command("serverStatus")
 #pprint(serverStatusResult)
+def listings(request):
+    testListing = {
 
-testListing = {
+	   'listing_id' : "00000001",
+	   'apartment_name' : "Gainesville Place",
+	   'num_bedrooms' : 4,
+	   'lister_name' : "MingYEEN"
+       }
 
-	'listing_id' : "00000001",
-	'apartment_name' : "Gainesville Place",
-	'num_bedrooms' : 4,
-	
-	'lister_name' : "MingYEEN" 
+    #result = db.listings.insert_one(testListing)
+    return render(request, "listing.html")
 
-}
+def home(request):
+    return render(request, "home.html")
 
-result = db.listings.insert_one(testListing)
-
+def view(request):
+    return render(request, "view.html")
--- sublease/sublease/urls.py
+++ sublease/sublease/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from proj import views
 
 urlpatterns = [
+    path("", views.home, name="home"),
+    path("listing/", views.listings, name="listings"),
+    path("view/", views.view, name="view"),
     path('admin/', admin.site.urls),
 ]
