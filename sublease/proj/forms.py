ab1b63e415f90e817a11f1e5758559164d09ac48
Mingyin Zhu <mzhu0114@gmail.com>
2020-02-01 00:35:31 -0500

    missing address updates

--- sublease/proj/forms.py
+++ sublease/proj/forms.py
@@ -1,12 +1,18 @@
 from django import forms
+import phonenumbers
+
 
 class getUserInfo(forms.Form):
     name = form.CharField(label = 'Name', widget = forms.TextInput(attrs = {'placeholder' = 'Name'}, max_length = 50))
-    phone = form.CharField(label = 'Phone', widget = forms.TextInput(attrs = {'placeholder' = 'Phone Number'}, max_length = 12))
-    #add in validation
+    phone1 = form.CharField(label = 'Area code', widget = forms.TextInput(attrs = {'placeholder' = '123'}, max_length = 3, min_length = 3))
+    phone2 = form.CharField(label = 'Number', widget = forms.TextInput(attrs = {'placeholder' = '456'}, max_length = 3, min_length = 3))
+    phone3 = form.CharField(label = 'Number', widget = forms.TextInput(attrs = {'placeholder' = '7890'}, max_length = 4, min_length = 4))
 
 class getApartmentInfo(forms.Form):
     address = form.CharField(label = 'Address', widget = forms.TextInput(attrs = {'placeholder' = 'Apartment Address'}))
-    #add in formatting
+    #Caleb updates
     complex = form.CharField(label = 'Complex', widget = forms.TextInput(attrs = {'placeholder' = 'Apartment Complex (Optional)'}, required = false))
-    bedrooms = form.CharField(label = 'Complex', widget = )
+    bedrooms = form.CharField(label = 'Bedrooms', widget = forms.TextInput(attrs = {'placeholder' = 'Number of Bedrooms'}, max_length = 1))
+    bathrooms = form.CharField(label = 'Bathrooms', widget = forms.TextInput(attrs = {'placeholder' = 'Number of Bathrooms', max_length = 1}))
+    cost = form.CharField(label = 'Cost', widget = forms.TextInput(attrs = {'placeholder' = 'Cost per Month'}))
+    utilities = form.BooleanField(label = 'Utilities', widget = )
