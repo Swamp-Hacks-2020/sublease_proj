456a31564006a3d566a2d5a91827a713a8d4ca59
rivera-fernando <f.rivera@ufl.edu>
2020-01-31 23:51:39 -0500

    added the uri

--- sublease/proj/views.py
+++ sublease/proj/views.py
@@ -2,10 +2,9 @@ from pymongo import MongoClient
 
 from pprint import pprint
 
-client = MongoClient()
+client = MongoClient("mongodb+srv://guest:root@listings-gru4r.mongodb.net/test?retryWrites=true&w=majority")
 
 db = client.admin
 
 serverStatusResult = db.command("serverStatus")
 pprint(serverStatusResult)
-
