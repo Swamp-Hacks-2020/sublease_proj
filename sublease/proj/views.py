501ab4b583458cd9e0341285b27a7c969782d36a
ZonshenYu <zonshenyu@gmail.com>
2020-02-01 01:33:53 -0500

    syntax forms fix

--- sublease/proj/views.py
+++ sublease/proj/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from pymongo import MongoClient
-
+from .forms import*
 from pprint import pprint
 
 client = MongoClient("mongodb+srv://guest:root@listings-gru4r.mongodb.net/test?retryWrites=true&w=majority")
@@ -10,16 +10,47 @@ db = client.business
 #serverStatusResult = db.command("serverStatus")
 #pprint(serverStatusResult)
 def listings(request):
-    testListing = {
 
-	   'listing_id' : "00000001",
-	   'apartment_name' : "Gainesville Place",
-	   'num_bedrooms' : 4,
-	   'lister_name' : "MingYEEN"
-       }
+	form_user = getUserInfo(request.POST or None)
+	form_apt = getApartmentInfo(request.POST or None)
+	
+	if form_user.is_valid():
+		name = form_user.cleaned_data['name']
+		phone1 = form_user.cleaned_data['phone1']
+		phone2 = form_user.cleaned_data['phone2']
+		phone3 = form_user.cleaned_data['phone3']
+		email = form_user.cleaned_data['email']
+	else:
+		name = ""
+		phone1 = ""
+		phone2 = ""
+		phone3 = ""
+		email = ""
 
-    #result = db.listings.insert_one(testListing)
-    return render(request, "listing.html")
+	if form_apt.is_valid():
+		street1 = form_user.cleaned_data['street1']
+		street2 = form_user.cleaned_data['street2']
+		city = form_user.cleaned_data['city']
+		zip = form_user.cleaned_data['zip']
+		semester = form_user.cleaned_data['semester']
+		complex = form_user.cleaned_data['complex']
+		bedrooms = form_user.cleaned_data['bedrooms']
+		bathrooms = form_user.cleaned_data['bathrooms']
+		cost = form_user.cleaned_data['cost']
+		utilities = form_user.cleaned_data['utilities']
+	else:
+		street1 = ""
+		street2 = ""
+		city = ""
+		zip = ""
+		semester = ""
+		complex = ""
+		bedrooms = ""
+		bathrooms = ""
+		cost = ""
+		utilities = ""
+
+	return render(request, "listing.html")
 
 def home(request):
     return render(request, "home.html")
