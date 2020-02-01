ee180be3544052d743592a2a5547656b639c980f
rivera-fernando <f.rivera@ufl.edu>
2020-02-01 10:19:57 -0500

    changed utilities to a text field for now, will try to make it work as a dropdown if time permits

--- sublease/proj/forms.py
+++ sublease/proj/forms.py
@@ -1,6 +1,11 @@
 from django import forms
 
 
+UTILITIES_CHOICES = [
+ ('Included', 'Included'),
+ ('Not Included', 'Not Included'),
+]
+
 class getUserInfo(forms.Form):
     name = forms.CharField(label = 'Name', widget = forms.TextInput(attrs = {'placeholder' : 'Name'}), max_length = 50)
     phone = forms.CharField(label = 'Phone Number', widget = forms.TextInput(attrs = {'placeholder' : '(000)-000-0000'}), max_length = 14)
@@ -10,10 +15,10 @@ class getApartmentInfo(forms.Form):
     street1 = forms.CharField(label = 'Street 1', widget = forms.TextInput(attrs = {'placeholder' : 'Street Address'}))
     street2 = forms.CharField(label = 'Street 2', widget = forms.TextInput(attrs = {'placeholder' : 'Apt, Bldg, Suite, etc.'}), required = False)
     city = forms.CharField(label = 'City', widget = forms.TextInput(attrs = {'placeholder' : 'Metropolis'}))
-    zip = forms.CharField(label = 'Zip', widget = forms.TextInput(attrs = {'placeholder' : '00000'}), max_length = 5)
+    zip = forms.CharField(label = 'Zip', widget = forms.TextInput(attrs = {'placeholder' : 'Zip (00000)'}), max_length = 5)
     semester = forms.CharField(label = 'Semester', widget = forms.TextInput(attrs = {'placeholder' : 'Semester Availabile (i.e. Fall 2020, Spring 2020, Summer 2020)'}), max_length = 15)
     complex = forms.CharField(label = 'Complex', widget = forms.TextInput(attrs = {'placeholder' : 'Apartment Complex'}))
     bedrooms = forms.CharField(label = 'Bedrooms', widget = forms.TextInput(attrs = {'placeholder' : 'Number of Bedrooms'}), max_length = 1)
     bathrooms = forms.CharField(label = 'Bathrooms', widget = forms.TextInput(attrs = {'placeholder' : 'Number of Bathrooms'}), max_length = 1)
     cost = forms.CharField(label = 'Cost', widget = forms.TextInput(attrs = {'placeholder' : 'Cost per Month'}))
-    utilities = forms.ChoiceField(choices = [("Included", "Included"), ("Not Included", "Not included")]) #need to check syntax
+    utilities = forms.CharField(label = 'Utilities', widget = forms.TextInput(attrs = {'placeholder' : 'Utilities Included (i.e. Yes, No)'}))
